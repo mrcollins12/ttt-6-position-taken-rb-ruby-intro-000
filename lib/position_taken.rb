b3c6b6244698a6951b8d605a1c08426969147056
Brandon Collins <brandon@flatironschool.com>
2017-09-27 19:48:28 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+board = ["X", "X", nil, "", "O", "X", "X", " ", "O"]
+
+def position_taken?(board, location)
+index = board[location.to_i - 1]
+
+index != " " && index != "" && index != nil
+end
+puts position_taken?(board, 8)
